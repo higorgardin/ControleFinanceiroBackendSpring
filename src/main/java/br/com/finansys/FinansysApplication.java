87ac6b8ab81542784220ee2da1e0d3cd646caed2
Higor Gardin <higorgardin00@gmail.com>
2022-06-07 22:56:07 -0300

    Update project info

---
+++ src/main/java/br/com/finansys/FinansysApplication.java
@@ -0,0 +1,13 @@
+package br.com.finansys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinansysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinansysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/finansys/FinansysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.finansys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinansysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
